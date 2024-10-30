4ab7d6735579b691e8caff1bc7c9b0d434ec26fc
jupac1053 <jupac1053799430@gmail.com>
2024-10-30 12:14:59 -0500

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ import { createClient } from 'https://cdn.skypack.dev/@supabase/supabase-js';
 // Configuración de Supabase
 const supabaseUrl = 'https://wakkxdrjrcsnsdnphupv.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indha2t4ZHJqcmNzbnNkbnBodXB2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAzMDcxMTgsImV4cCI6MjA0NTg4MzExOH0.09LsLeJ_-jQ6DvSP1J2EWSkcExeJm5TE0Ty1_bCZDvA'; // anon (pública)
-const supabaseSecret = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indha2t4ZHJqcmNzbnNkbnBodXB2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAzMDcxMTgsImV4cCI6MjA0NTg4MzExOH0.09LsLeJ_-jQ6DvSP1J2EWSkcExeJm5TE0Ty1_bCZDvA';
+const supabaseSecret = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indha2t4ZHJqcmNzbnNkbnBodXB2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMDMwNzExOCwiZXhwIjoyMDQ1ODgzMTE4fQ.IzrUgVzfCO8uk5N0YiB_L9eKN_cyUTSWBVWNkmPMSKc';
 // Inicializa Supabase
 const supabase = createClient(supabaseUrl, supabaseKey);
 
